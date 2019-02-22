e89593dac8e6bfe3eb46c4b87391e1a692f2052b
Emmanuel Tissera <emmanuel.tissera@gmail.com>
2019-02-22 17:26:28 +1100

    Added more changes found during 3-step run through

---
+++ Tutorial/step-2/TeePhrase/Controllers/BlogIndexController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TeePhrase.Models;
+using Umbraco.Headless.Client.Net.Services;
+using Umbraco.Headless.Client.Net.Web;
+
+namespace TeePhrase.Controllers
+{
+    public class BlogIndexController : BaseContentItemController
+    {
+        public BlogIndexController(UmbracoContext umbracoContext, PublishedContentService publishedContentService) : base(umbracoContext, publishedContentService)
+        {
+        }
+
+        public override Task<IActionResult> Index()
+        {
+            return RenderContentItem<BlogIndex>();
+        }
+    }
+}
--- Tutorial/step-2/TeePhrase/Controllers/HomeController.cs
+++ Tutorial/step-2/TeePhrase/Controllers/HomeController.cs
@@ -6,25 +6,17 @@ using Umbraco.Headless.Client.Net.Web;
 
 namespace TeePhrase.Controllers
 {
-    public class HomeController : DefaultUmbracoController
+    public class HomeController : BaseContentItemController
     {
-        public HomeController(UmbracoContext umbracoContext, PublishedContentService headlessService) : base(
-            umbracoContext, headlessService)
+        public HomeController(UmbracoContext umbracoContext, PublishedContentService publishedContentService) : base(umbracoContext, publishedContentService)
         {
         }
 
         public override Task<IActionResult> Index()
         {
-
-            // get the content for the current route
-            var content = UmbracoContext.GetContent(false);
-
-            // map the ContentItem to a custom model called Home
-            var model = HeadlessService.MapTo<Home>(content);
-
-            // return the view which will be located at
-            return Task.FromResult((IActionResult)View(model));
+            return RenderContentItem<Home>();
         }
+
     }
 
 }
